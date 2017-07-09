80f5f94442cf4021841e5fa3b76b4ca6ef2e703c
brunocho <chohj850930@gmail.com>
2017-07-09 13:05:40 +0900

    js

---
+++ js/app.js
@@ -0,0 +1,54 @@
+// var weaponsList = ["gun","knife","punch"];
+// 
+// var soldier ={
+// 	name: "bruno",
+// 	gender: "male",
+// 	country: "Korea",
+// 	favorite: "freedom",
+// 	weapon: weaponsList[2]
+// }
+// 
+// function add(one,two){
+// 	return one+two;
+// }
+// 
+// function subtract(sun,moon) {
+// 	return sun-moon;
+// }
+// 
+// var age=prompt("what is your age?")
+// 
+// if(age<18){
+// 	console.log("You can't drink that beer")
+// } else if(age>18 && age<40){
+// 	console.log("cheers!")
+// } else if(age>40 && age<60){
+// 	console.log("hey dad")
+// } else {
+// 	console.log("die")
+// }
+
+
+
+
+
+$(document).ready(function(){
+
+	var $heart = $('.heart');
+
+	$heart.click(function(){
+		$(this).toggleClass('fa-heart-o fa-heart')
+	})
+
+})
+
+
+
+
+
+
+
+
+
+
+
